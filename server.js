3d50746400958b9b60162561b7c400b76f1eac6c
TaVon <tsyuille@gmail.com>
2022-06-15 20:17:48 -0400

    Remove delete option, fix post method, and add css file

--- server.js
+++ server.js
@@ -33,18 +33,9 @@ app.get('/', (req, res) => {
 
 app.post('/addTea', (req, res) => {
     db.collection('tea').insertOne(req.body)
-    .then(res => {
+    .then(result => {
         console.log('Tea Added')
         res.redirect('/')
     })
     .catch(error => console.error(error))
 })
-
-/* app.delete('/deleteTea', (req, res) => {
-    db.collection('tea').deleteOne({ name: req.body.name })
-    .then(res => {
-        console.log('Tea deleted')
-        res.json('Tea deleted')
-    })
-    .catch(error => console.error(error))
-}) */
